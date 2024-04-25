dec744561db6c4a0dfb60d6b0ea8b17533e07f99
quluk <143124115+quluk@users.noreply.github.com>
2024-04-25 11:28:29 -0700

    Update RandomLetterChooser.java

--- src/main/java/RandomLetterChooser.java
+++ src/main/java/RandomLetterChooser.java
@@ -1,20 +1,22 @@
 public class RandomLetterChooser extends RandomStringChooser
 {
-	
-	public RandomLetterChooser(String str)
-	{ 
-		/* to be implemented in part (b) */
-		
-	}
-	
-	
-	public static String[] getSingleLetters(String str)
-	{ 
-		String[] letterArray = new String[str.length()];
-		for(int i=0; i<str.length(); i++)
-		{
-			letterArray[i] = ""+str.charAt(i);
-		}
-		return letterArray;
-	}
+
+public RandomLetterChooser(String str){
+super(getSingleLetters(str));
+
 }
+
+
+public static String[] getSingleLetters(String str)
+{
+String[] letterArray = new String[str.length()];
+for(int i=0; i<str.length(); i++)
+{
+letterArray[i] = ""+str.charAt(i);
+}
+return letterArray;
+}
+}
+
+
+
